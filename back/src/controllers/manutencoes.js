8009791643c096f2ee1c42a222326b4900c22300
RodrickCS <caetanorodrigo46@gmail.com>
2023-02-22 09:36:38 -0300

    .

--- back/src/controllers/manutencoes.js
+++ back/src/controllers/manutencoes.js
@@ -5,7 +5,10 @@ require("dotenv").config();
 
 const create = async (req, res) => {
   try {
-    if (Object.keys(req.body).length === 5) {
+    if (
+      Object.keys(req.body).length === 5 ||
+      Object.keys(req.body).length === 4
+    ) {
       let manutencao = await prisma.manutencoes.create({
         data: req.body,
       });
@@ -25,6 +28,22 @@ const read = async (req, res) => {
     res.status(200).json(manutencao).end();
   } catch (err) {
     res.status(500).json(err).end();
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    if (Object.keys(req.body).length > 0) {
+      let manutencao = await prisma.manutencoes.update({
+        data: req.body,
+      });
+      res.status(204).json({ msg: "Criado" });
+    } else {     
+      res.status(400).json({ msg: "Formulário inválido" });
+    }
+  } catch (err) {
+    res.status(500).json(err).end();
+    console.log(err);
   }
 };
 
