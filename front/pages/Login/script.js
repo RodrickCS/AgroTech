372889c80387c23a622ba5059d3161ea1075928b
RodrickCS <caetanorodrigo46@gmail.com>
2023-03-02 10:23:56 -0300

    .

--- front/pages/Login/script.js
+++ front/pages/Login/script.js
@@ -3,29 +3,34 @@ const inpPasswd = document.getElementById("inpPasswd");
 uriLogin = "http://localhost:3000/funcionarios/login";
 
 const login = () => {
-  let form = {
-    email: inpEmail.value,
-    senha: inpPasswd.value,
-  };
+  if (inpEmail.value != "" && inpPasswd.value != "") {
+    let form = {
+      email: inpEmail.value,
+      senha: inpPasswd.value,
+    };
 
-  options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(form),
-  };
-  fetch(uriLogin, options)
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      if (data.length == 2) {
-        alert("redirecting...");
-        localStorage.setItem("token", JSON.stringify(data[0].token));
-        localStorage.setItem("role", JSON.stringify(data[1].role));
+    options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    };
+    fetch(uriLogin, options)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        if (data.length == 2) {
+          alert("redirecting...");
+          localStorage.setItem("token", JSON.stringify(data[0].token));
+          localStorage.setItem("role", JSON.stringify(data[1].role));
 
-        window.location.href = "../gerenciamento/index.html";
-      }
-    });
+          window.location.href = "../gerenciamento/index.html";
+        }
+      });
+  } else {
+    alert("Preencha todos os campos!");
+
+  }
 };
