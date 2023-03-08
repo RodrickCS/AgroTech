7f210fb261ddac5db8a491283225896cee6f07b7
RodrickCS <caetanorodrigo46@gmail.com>
2023-03-08 07:50:38 -0300

    .

--- back/src/routes/viagens.js
+++ back/src/routes/viagens.js
@@ -6,8 +6,8 @@ const Middleware = require("../middleware/middleware");
 
 const viagens = require("../controllers/viagens");
 
-router.get("/read", viagens.read)
-router.post("/create", viagens.create)
-router.put("/update/:id_viagem", viagens.updateChegou)
+router.get("/read", viagens.read);
+router.post("/create", viagens.create);
+router.put("/update/:id_viagem", Middleware.validaAcesso, viagens.updateChegou);
 
 module.exports = router;
